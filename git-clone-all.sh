1bdcd479f0256aa65a89cf1984c9adf3bf5bef96
makrushinoff <66564178+makrushinoff@users.noreply.github.com>
2022-01-06 22:43:27 +0200

    Upload git-clone-all file

---
+++ git-clone-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Clonning all repositories..."
+
+git clone https://github.com/WebMuffins/rt-board.git
+git clone https://github.com/WebMuffins/rt-security.git
+git clone https://github.com/WebMuffins/rt-gateway.git
+git clone https://github.com/WebMuffins/rt-discovery-server.git
+git clone https://github.com/WebMuffins/rt-messenger.git
+
+echo "Clonning completed"
